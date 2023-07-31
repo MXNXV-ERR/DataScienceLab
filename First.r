f76f977febaef6a90270f1f998ba6df4f6ec44b4
Manav R <67834464+MXNXV-ERR@users.noreply.github.com>
2023-07-31 12:29:47 +0530

    Update First.r

--- First.r
+++ First.r
@@ -1,3 +1,6 @@
+install.packages("ggplot2") # If not already installed
+library(ggplot2)
+
 for(x in 1:10)
   print(x)
 
@@ -5,18 +8,25 @@ x <- c(5,7,8,7,2,2,9,4,11,12,9,6)
 y <- c(99,86,87,88,111,103,87,94,78,77,85,86)
 
 
-plot(1, 3) 
-plot(x, y) 
+plot(1, 3)
+plot(x, y)
 
-plot(1:10, type="l") 
+plot(1:10, type="l")
 
 data1 <- read.csv(file.choose(), header=T)
 print(data1)
 
 df<-data.frame(Gender)
 
-install.packages("ggplot2") # If not already installed
-library(ggplot2)
+
+ggplot(data1,aes(x=Gender,fill=Level.of.Education))+
+  geom_bar(position='dodge')+
+  labs(title='Gender vs LOE')
+
+ggplot(data1, aes(x = Age)) +labs(title = "Age")+geom_boxplot(outlier.size=2)
+
+ggplot(data1, aes(x = Age)) +geom_freqpoly(bins=10)
+
 
 ggplot(data1,aes(x=Gender,fill=Level.of.Education))+
   geom_bar(position='dodge')+
